9ef98910f55bef18f4417f32e9a27cdad28b5016
valerianEhret <vkadirovv@gmail.com>
2021-10-21 22:31:03 +0200

    Updates

---
+++ precache-manifest.9cfa9c31a904bebca64f3267189eb2c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ed5e184abd96b15524ee85e1521322",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "c3043a057e219fa28d6b",
+    "url": "/todolist/static/css/main.aa207b39.chunk.css"
+  },
+  {
+    "revision": "d3af9dca06810be76bfb",
+    "url": "/todolist/static/js/2.fc74ac89.chunk.js"
+  },
+  {
+    "revision": "e2896a34bc173bbd7b6d83453e91d5e1",
+    "url": "/todolist/static/js/2.fc74ac89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3043a057e219fa28d6b",
+    "url": "/todolist/static/js/main.8c461c03.chunk.js"
+  },
+  {
+    "revision": "c3d5ccae1ee3a7bf91ff",
+    "url": "/todolist/static/js/runtime-main.16223656.js"
+  }
+]);
